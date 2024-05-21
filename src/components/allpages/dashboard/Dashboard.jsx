6b6478507fede089be38fbf0aea4d6f879ee3917
asdasdf <asdasffas@gmail.com>
2024-05-21 22:08:23 +0300

    Commit

--- src/components/allpages/dashboard/Dashboard.jsx
+++ src/components/allpages/dashboard/Dashboard.jsx
@@ -3,13 +3,7 @@ import React from 'react'
 function Dashboard() {
   return (
     <div className=' w-[12rem] h-44 bg-white text-white'>
-      Dashboard
-      Dashboard
-      Dashboard
-      Dashboard
-      Dashboard
-      Dashboard
-      Dashboard
+     
     </div>
   )
 }
